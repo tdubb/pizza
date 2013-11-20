113552e158d0a2f0a754b73619098b1a5ade80a0
MakerSquare Student <students@themakersquare.com>
2013-11-20 11:17:48 -0600

    Add vegetarian boolean keyword to Toppings initialize method.

--- pizza.rb
+++ pizza.rb
@@ -4,9 +4,10 @@ module Pizza
   end
 
   class Topping
-  	attr_accessor :name
-  	def initialize(name)
+  	attr_accessor :name :vegetarian
+  	def initialize(name, vegetarian: false)
   		@name = name
+  		@vegetarian = vegetarian
   	end
 
   end
